234aa48fc35da39f861f31a13203289e5805485e
Artem Terzi <terzi.artem.yuriyovych@gmail.com>
2023-05-24 19:24:51 +0300

    CHANGED: AuthSlice optimized

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -1,16 +1,22 @@
-const { createSlice } = require('@reduxjs/toolkit');
+const { createSlice, isAnyOf } = require('@reduxjs/toolkit');
 const { register, signIn, logout, refreshUser } = require('./operations');
 
-// const handlePendidng = state => {};
+const handlePendidng = state => {
+  state.isRefreshing = true;
+};
 
 const handleFulfilled = (state, { payload }) => {
   state.user = payload.user;
   state.token = payload.token;
   state.isLoggedIn = true;
   state.isRefreshing = false;
+  state.error = null;
 };
 
-// const handleRejected = (state, { error: { message } }) => {};
+const handleRejected = (state, { error: { message } }) => {
+  state.error = message;
+  state.isRefreshing = false;
+};
 
 const authSlice = createSlice({
   name: 'auth',
@@ -19,30 +25,40 @@ const authSlice = createSlice({
     token: null,
     isLoggedIn: false,
     isRefreshing: false,
+    error: null,
   },
   extraReducers: builder => {
     builder
       .addCase(register.fulfilled, handleFulfilled)
-      .addCase(register.rejected, (_, { payload }) => {
-        console.log(payload);
-      })
       .addCase(signIn.fulfilled, handleFulfilled)
       .addCase(logout.fulfilled, state => {
         state.user = { name: null, email: null };
         state.token = null;
         state.isLoggedIn = false;
       })
-      .addCase(refreshUser.pending, state => {
-        state.isRefreshing = true;
-      })
       .addCase(refreshUser.fulfilled, (state, { payload }) => {
         state.user = payload;
         state.isLoggedIn = true;
         state.isRefreshing = false;
       })
-      .addCase(refreshUser.rejected, state => {
-        state.isRefreshing = false;
-      });
+      .addMatcher(
+        isAnyOf(
+          logout.pending,
+          signIn.pending,
+          register.pending,
+          refreshUser.pending
+        ),
+        handlePendidng
+      )
+      .addMatcher(
+        isAnyOf(
+          register.rejected,
+          logout.rejected,
+          signIn.rejected,
+          refreshUser.rejected
+        ),
+        handleRejected
+      );
   },
 });
 
